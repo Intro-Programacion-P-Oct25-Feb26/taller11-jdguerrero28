9def4bbec95e0d5e524d1e348538f93fe71a2c1d
jdguerrero28 <jdiegogueal@gmail.com>
2026-01-08 17:09:22 -0500

    Problema5

--- ejercicios/Problemas/src/paquete05/Problema5.java
+++ ejercicios/Problemas/src/paquete05/Problema5.java
@@ -10,5 +10,34 @@ package paquete05;
  * @author reroes
  */
 public class Problema5 {
-    
+
+    public static void main(String[] args) {
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+
+        int[][] resultado = sumarMatrices(informacion, informacion2);
+        imprimirMatriz(resultado);
+
+    }
+
+    public static int[][] sumarMatrices(int[][] a, int[][] b) {
+        int[][] suma = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                suma[i][j] = a[i][j] + b[i][j];
+            }
+
+        }
+        return suma;
+    }
+
+    public static void imprimirMatriz(int[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.printf("%s\t", m[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+
 }
